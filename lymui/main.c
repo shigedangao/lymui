28d984a31550e1450298c5442dd5951670257354
Marc Intha-amnouay <marc.inthaamnouay@gmail.com>
2018-02-05 13:44:06 +0100

    Initial Commit

---
+++ lymui/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lymui
+//
+//  Created by Marc on 05/02/2018.
+//  Copyright © 2018 Marc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
