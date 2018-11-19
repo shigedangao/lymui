17a4f1810ef4371fe3dbe8ad547e61fd7f9fbc48
Yellowman <marc.inthaamnouay@gmail.com>
2018-11-19 13:59:56 +0100

    :white_check_mark: wip test luv

--- tests/luv.test.js
+++ tests/luv.test.js
@@ -4,8 +4,8 @@ const lib = require('../build/Debug/lymuilib');
 
 SegfaultHandler.registerHandler('lch_convert.log');
 
-describe('Creating LCH from RGB', () => {
-  it('Expect to create LCH from RGB with type SRGB', async () => {
+describe('Creating LUV from RGB', () => {
+  it('Expect to create LUV from RGB with type SRGB', async () => {
     const rgb = {
       r: 5,
       g: 10,
@@ -25,7 +25,7 @@ describe('Creating LCH from RGB', () => {
     });
   });
 
-  it('Expect to create LCH from RGB with type ARGB', async () => {
+  it('Expect to create LUV from RGB with type ARGB', async () => {
     const rgb = {
       r: 5,
       g: 10,
@@ -45,11 +45,11 @@ describe('Creating LCH from RGB', () => {
     });
   });
 
-  it('Expect to create a valid LCH from a white RGB', async () => {
+  it('Expect to create a valid LUV from a white RGB', async () => {
     const rgb = {
-      r: 100,
-      g: 100,
-      b: 100
+      r: 255,
+      g: 255,
+      b: 255
     };
 
     const luv = await lib.convert({
@@ -59,13 +59,13 @@ describe('Creating LCH from RGB', () => {
     });
 
     expect(luv).to.be.deep.equal({
-      l: 42.3707,
-      u: 0.0006,
-      v: -0.0039
+      l: 99.9922,
+      u: 0.0016,
+      v: -0.009
     });
   });
 
-  it('Expect to create a valid LCH from a white RGB', async () => {
+  it('Expect to create a valid LUV from a white RGB', async () => {
     const rgb = {
       r: 255,
       g: 255,
@@ -82,6 +82,38 @@ describe('Creating LCH from RGB', () => {
       l: 99.9922,
       u: 0.0016,
       v: -0.009
+    });
+  });
+
+  it('Expect to throw when a params is missing on the Rgb object', async () => {
+    try {
+      await lib.convert({
+        output: 'luv',
+        colorType: 'srgb'
+      });
+    } catch (e) {
+      expect(e).to.be.deep.equal({
+        err: 'Property not found in JS Object'
+      });
+    }
+  });
+
+  it('Expect to throw when a params is missing on the Rgb object', async () => {
+    const xyz = {
+      x: 0.03628,
+      y: 0.01777,
+      z: 0.11705
+    };
+    
+    const luv = await lib.convert({
+      data: xyz,
+      output: 'luv',
+    });
+
+    expect(luv).to.be.deep.equal({
+      l: 14.2705,
+      u: 4.4641,
+      v: -41.5165
     });
   });
 });
