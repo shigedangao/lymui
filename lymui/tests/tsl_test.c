a36f021bfb2be3484653c6ee26124928f7240c07
Yellowman <marc.inthaamnouay@gmail.com>
2019-01-05 23:58:37 +0100

    add support of tsl -> rgb

--- lymui/tests/tsl_test.c
+++ lymui/tests/tsl_test.c
@@ -41,7 +41,7 @@ ctest_return_t testBlackRgbFromTsl(ctest_t *test, void *arg) {
     free(tsl);
 }
 
-ctest_return_t testRgbFromTsl(ctest_t *test, void *arg) {
+ctest_return_t testTslFromRgb(ctest_t *test, void *arg) {
     Rgb *rgb = malloc(sizeof(Rgb));
     rgb->r = 50;
     rgb->g = 10;
@@ -56,17 +56,67 @@ ctest_return_t testRgbFromTsl(ctest_t *test, void *arg) {
     free(tsl);
 }
 
+ctest_return_t testRgbFromTsl(ctest_t *test, void *arg) {
+    Tsl *tsl = malloc(sizeof(Tsl));
+    tsl->t = 0.787;
+    tsl->s = 0.386;
+    tsl->l = 35.412;
+    
+    Rgb *rgb = getRgbFromTsl(tsl);
+    
+    CTAssertEqual(test, rgb->r, 50, "Expect R to be equal to %i but got %i", 50, rgb->r);
+    CTAssertEqual(test, rgb->g, 10, "Expect G to be equal to %i but got %i", 10, rgb->g);
+    CTAssertEqual(test, rgb->b, 128, "Expect B to be equal to %i but got %i", 128, rgb->b);
+
+    free(rgb);
+}
+
+ctest_return_t testRgbFromBrightTsl(ctest_t *test, void *arg) {
+    Tsl *tsl = malloc(sizeof(Tsl));
+    tsl->t = 0.0;
+    tsl->s = 0.0;
+    tsl->l = 255.0;
+    
+    Rgb *rgb = getRgbFromTsl(tsl);
+    
+    CTAssertEqual(test, rgb->r, 255, "Expect R to be equal to %i but got %i", 255, rgb->r);
+    CTAssertEqual(test, rgb->g, 255, "Expect G to be equal to %i but got %i", 255, rgb->g);
+    CTAssertEqual(test, rgb->b, 255, "Expect B to be equal to %i but got %i", 255, rgb->b);
+    
+    free(rgb);
+}
+
+ctest_return_t testRgbFromDarkTsl(ctest_t *test, void *arg) {
+    Tsl *tsl = malloc(sizeof(Tsl));
+    tsl->t = 0.0;
+    tsl->s = 0.0;
+    tsl->l = 0.0;
+    
+    Rgb *rgb = getRgbFromTsl(tsl);
+    
+    CTAssertEqual(test, rgb->r, 0, "Expect R to be equal to %i but got %i", 0, rgb->r);
+    CTAssertEqual(test, rgb->g, 0, "Expect G to be equal to %i but got %i", 0, rgb->g);
+    CTAssertEqual(test, rgb->b, 0, "Expect B to be equal to %i but got %i", 0, rgb->b);
+    
+    free(rgb);
+}
+
 ctcase_t *wrapTslCreationTest() {
     ctcase_t *tslCase = ctcase("Tsl color case");
     
     ctest_t *whiteTsl = ctest("White Rgb -> TSL case", testWhiteRgbFromTsl, NULL);
     ctest_t *blackTsl = ctest("Black Rgb -> TSL case", testBlackRgbFromTsl, NULL);
-    ctest_t *regularTsl = ctest("Regular Rgb -> TSL case", testRgbFromTsl, NULL);
-
+    ctest_t *regularTsl = ctest("Regular Rgb -> TSL case", testTslFromRgb, NULL);
+    ctest_t *tslToRgb = ctest("Create RGB from TSL", testRgbFromTsl, NULL);
+    ctest_t *tslToBrightRgb = ctest("Create White RGB from Bright TSL", testRgbFromBrightTsl, NULL);
+    ctest_t *tslToDarkRgb = ctest("Create Black RGB from Dark TSL", testRgbFromDarkTsl, NULL);
     
     ctctestadd(tslCase, whiteTsl);
     ctctestadd(tslCase, blackTsl);
     ctctestadd(tslCase, regularTsl);
+    ctctestadd(tslCase, tslToRgb);
+    ctctestadd(tslCase, tslToBrightRgb);
+    ctctestadd(tslCase, tslToDarkRgb);
     
     return tslCase;
 }
--- lymui/tsl.c
+++ lymui/tsl.c
@@ -9,14 +9,22 @@
 #include <stdlib.h>
 #include <math.h>
 #include "tsl.h"
+#include "helper.h"
 #include "errors.h"
+
+#define X_FORMULA(t) { -1.0 / tan(2.0 * M_PI * t) }
 
 /**
  * @brief Calculate Delta
  * @param c double
+ * @param op uint8_t
  * @return delta double
  */
-static double calculateDelta(double c) {
+static double calculateDelta(double c, uint8_t op) {
+    if (op) {
+        return c + (1.0 / 3.0);
+    }
+    
     return c - (1.0 / 3.0);
 }
 
@@ -26,6 +34,7 @@ static double calculateDelta(double c) {
  * @param r double
  * @param g double
  * @param b double
+ * @return c double
  */
 static double calculateNorm(double c, double r, double g, double b) {
     return c / (r + g + b);
@@ -54,8 +63,8 @@ Tsl *getTslFromRgb(Rgb *rgb) {
     
     double rNorm  = calculateNorm(_r, _r, _g, _b);
     double gNorm  = calculateNorm(_g, _r, _g, _b);
-    double rDelta = calculateDelta(rNorm);
-    double gDelta = calculateDelta(gNorm);
+    double rDelta = calculateDelta(rNorm, 0);
+    double gDelta = calculateDelta(gNorm, 0);
     double d_pi = 1.0 / (M_PI * 2.0);
     
     if (gDelta > 0.0) {
@@ -69,5 +78,67 @@ Tsl *getTslFromRgb(Rgb *rgb) {
     tsl->s = sqrt((9.0 / 5.0) * (powf(rDelta, 2.0) + powf(gDelta, 2.0)));
     tsl->l = 0.299 * _r + 0.587 * _g + 0.114 * _b;
     
+    free(rgb);
+    
     return tsl;
+}
+
+/////////////// static method for converting TSL -> RGB
+
+/**
+ * @brief Calculate G Delta (based on the answer of SergGr on Stackoverflow. See .h file for the link to the answer)
+ * @param tsl Tsl
+ * @return g double
+ */
+static double calculateGDelta(Tsl *tsl) {
+    double x = X_FORMULA(tsl->t);
+    double g = sqrt(5.0 / (x * x + 1.0)) / 3.0 * tsl->s;
+    
+    if (tsl->t > 0.5) {
+        return -g;
+    } else if (tsl->t < 0.5) {
+        return g;
+    } else {
+        return g;
+    }
+}
+
+/**
+ * @brief Calculate R Delta
+ * @param tsl Tsl
+ * @param g double
+ * @return r double
+ */
+static double calculateRDelta(Tsl *tsl, double g) {
+    if (!tsl->t) {
+        return sqrt(5.0) / 3.0 * tsl->s;
+    }
+    
+    double x = X_FORMULA(tsl->t);
+    return x * g;
+}
+
+Rgb *getRgbFromTsl(Tsl *tsl) {
+    Rgb *rgb = initRgb();
+    if (rgb == NULL) {
+        return NULL;
+    }
+    
+    if (tsl == NULL) {
+        rgb->error = NULL_INPUT_PARAM;
+        return rgb;
+    }
+    
+    double gDelta = calculateGDelta(tsl);
+    double _r = calculateDelta(calculateRDelta(tsl, gDelta), 1);
+    double _g = calculateDelta(gDelta, 1);
+    double _k = tsl->l / (0.185 * _r + 0.473 * _g + 0.114);
+    
+    rgb->r = doubleToUint(_k * _r);
+    rgb->g = doubleToUint(_k * _g);
+    rgb->b = doubleToUint(_k * (1 - _r - _g));
+    
+    free(tsl);
+    
+    return rgb;
 }
