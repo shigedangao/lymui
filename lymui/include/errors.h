f58cd73c940a3914b098c1c9ff1beb07616618eb
Yellowman <marc.inthaamnouay@gmail.com>
2019-03-11 22:54:10 +0100

    update doc & remove unused error

--- lymui/include/errors.h
+++ lymui/include/errors.h
@@ -9,7 +9,6 @@
 #ifndef errors_h
 #define errors_h
 
-#define NULL_INPUT_STRUCT "The input struct is empty"
 #define NULL_INPUT_PARAM "The imput param is empty"
 #define WRONG_INPUT_PARAM "The input param is incorrect"
 #define MALLOC_ERROR "Unable to allocate memory for creating data"
